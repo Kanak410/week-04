255c489dda9db9cffaa28c092301c82fada2f3ea
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-13 00:26:23 +0530

    [kanak rajput] added file of map interface of merge map

---
+++ Day2_Collections/src/main/java/org/example/mapinterface/groupobjectsbyProperty/Employee.java
@@ -0,0 +1,4 @@
+package org.example.mapinterface.groupobjectsbyProperty;
+
+public class Employee {
+}
--- Day2_Collections/src/main/java/org/example/mapinterface/mergeTwoMaps/Merge.java
+++ Day2_Collections/src/main/java/org/example/mapinterface/mergeTwoMaps/Merge.java
@@ -1,4 +1,21 @@
 package org.example.mapinterface.mergeTwoMaps;
 
+import java.util.HashMap;
+import java.util.Map;
 public class Merge {
+public static Map<String,Integer>mer(Map<String,Integer>m1,Map<String,Integer>m2){
+    Map<String,Integer>res=new HashMap<>(m1);
+    for(Map.Entry<String,Integer>entry: m2.entrySet()){
+        res.put(entry.getKey(), res.getOrDefault(entry.getKey(),0)+ entry.getValue());
+    }
+    return res;
+}
+
+
+    public static void main(String[] args) {
+        Map<String,Integer>map1= Map.of("A",1,"B",2);
+        Map<String,Integer>map2=Map.of("B",3,"C",4);
+        Map<String,Integer>mergemap=mer(map1,map2);
+        System.out.println(mergemap);
+    }
 }
