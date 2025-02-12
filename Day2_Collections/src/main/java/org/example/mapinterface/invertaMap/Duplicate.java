fd1efc42590678f461058d98438442d1541ec8a8
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-12 23:45:33 +0530

    [kanak rajput] added file of map interface of inverted map

--- Day2_Collections/src/main/java/org/example/mapinterface/invertaMap/Duplicate.java
+++ Day2_Collections/src/main/java/org/example/mapinterface/invertaMap/Duplicate.java
@@ -1,4 +1,33 @@
 package org.example.mapinterface.invertaMap;
+import java.util.*;
 
 public class Duplicate {
+    public static <K, V> Map<V, List<K>> invert(Map<K, V> inputMap) {
+        Map<V, List<K>> invertedMap = new HashMap<>();
+
+        for (Map.Entry<K, V> entry : inputMap.entrySet()) {
+            V value = entry.getValue();
+            K key = entry.getKey();
+
+
+            if (!invertedMap.containsKey(value)) {
+                invertedMap.put(value, new ArrayList<>());
+            }
+            invertedMap.get(value).add(key);
+        }
+
+        return invertedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 1);
+        inputMap.put("B", 2);
+        inputMap.put("C", 1);
+
+        Map<Integer, List<String>> result = invert(inputMap);
+        System.out.println(result);
+    }
 }
+
+
