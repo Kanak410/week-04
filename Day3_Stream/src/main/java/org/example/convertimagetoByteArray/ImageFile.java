d5d0ddc565371dc58590c84c4cfd555fbcc32e50
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-13 23:30:39 +0530

    [kanak]added a file of updated convertimage

--- Day3_Stream/src/main/java/org/example/convertimagetoByteArray/ImageFile.java
+++ Day3_Stream/src/main/java/org/example/convertimagetoByteArray/ImageFile.java
@@ -9,23 +9,21 @@ import java.util.Arrays;
 
 public class ImageFile {
     public static void main(String[] args) {
-        String imagePath = "/Users/siddhu/Desktop/cg-tut/Week4/Day03-IOStreams/src/main/resources/cat-sunglasses" +
-                "-gangster-digital-art-animal-4k-wallpaper-uhdpaper.com-241@0@j.jpg";
-        // image path -> Ye hamare source image ka path h jisme ek image input dena hota hai
-        String newImagePath = "/Users/siddhu/Desktop/cg-tut/Week4/Day03-IOStreams/src/main/resources/cat.jpg";
-        // new Image Path -> ye ek empty cat.jpg name ka file add krna h jisme apan write krnge image ko
+        String imagePath = "D:\\week-04\\Day3_Stream\\src\\main\\java\\org\\example\\convertimagetoByteArray\\image1.jpg";
+
+        String newImagePath = "D:\\week-04\\Day3_Stream\\src\\main\\java\\org\\example\\convertimagetoByteArray\\copy.jpg";
 
         try{
             byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
-            try(ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);// iss line me apan
-                // ne read kiya unnn bytes ko jo image Bytes me store hai
+            try(ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()){
                 int byteData;
                 while((byteData = byteArrayInputStream.read())!= -1){
                     byteArrayOutputStream.write(byteData);
                 }
 
-                byte[] newImageBytes = byteArrayOutputStream.toByteArray();// ab jo data output stream me store hai
+                byte[] newImageBytes = byteArrayOutputStream.toByteArray();
 
                 try(FileOutputStream fileOutputStream = new FileOutputStream(newImagePath)){
                     fileOutputStream.write(newImageBytes);
