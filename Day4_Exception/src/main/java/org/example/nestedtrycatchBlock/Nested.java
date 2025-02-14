b8769e1ee3d97e619d9a74e3f6084e06e8f877bc
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-14 22:33:40 +0530

    [kanak]added a file of bank system

--- Day4_Exception/src/main/java/org/example/nestedtrycatchBlock/Nested.java
+++ Day4_Exception/src/main/java/org/example/nestedtrycatchBlock/Nested.java
@@ -1,4 +1,34 @@
 package org.example.nestedtrycatchBlock;
 
+import java.util.Scanner;
+
 public class Nested {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try{
+            System.out.println("enter size");
+            int n= sc.nextInt();
+            int [] array=new int[n];
+            System.out.println("enter the index");
+            int ind= sc.nextInt();
+            System.out.println("enter the diuisor");
+            int div=sc.nextInt();
+            try{
+                int ele=array[ind];
+                try{
+                    int res=ele/div;
+                    System.out.println("result"+res);
+                }catch (ArithmeticException e){
+                    System.out.println("cannot divide");
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("invalid index");
+            }
+        }catch (Exception e){
+            System.out.println("error");
+        }finally {
+            sc.close();
+        }
+
+    }
 }
