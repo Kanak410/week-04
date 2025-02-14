f4fac932b8c1e1159820569a67aa9c4e28614a91
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-14 16:32:28 +0530

    [kanak]added the file of exception methods

--- Day4_Exception/src/main/java/org/example/methodsforpropagation/Methods.java
+++ Day4_Exception/src/main/java/org/example/methodsforpropagation/Methods.java
@@ -1,4 +1,18 @@
 package org.example.methodsforpropagation;
-
+import java.io.*;
+import java.util.*;
 public class Methods {
+    public static void main(String[] args) {
+       try{
+           method2();
+       }catch (ArithmeticException e){
+           System.out.println("Handled exception in main");
+       }
+    }
+    public static void method2(){
+        method1();
+    }
+    public static void method1(){
+        throw new ArithmeticException("10/0 b cause exception");
+    }
 }
---
+++ Day4_Exception/src/main/java/org/example/nestedtrycatchBlock/Nested.java
@@ -0,0 +1,4 @@
+package org.example.nestedtrycatchBlock;
+
+public class Nested {
+}
