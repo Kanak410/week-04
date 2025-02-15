ff6c8f676edb03041a286185a13d31a2c8fd6359
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-15 14:57:10 +0530

    [kanak]added a program of regex of valid plate number

---
+++ Day5_RegexAndJunit/src/main/java/com/regex/licenseplateNumber/Number.java
@@ -0,0 +1,16 @@
+package com.regex.licenseplateNumber;
+import java.util.Scanner;
+import java.util.regex.*;
+public class Number {
+    public static boolean licence(String number){
+        String regex="^[A-Z]{2}\\d{4}$";
+        return Pattern.matches(regex,number);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number: ");
+        String number=sc.nextLine();
+        System.out.println(licence(number));
+
+    }
+}
