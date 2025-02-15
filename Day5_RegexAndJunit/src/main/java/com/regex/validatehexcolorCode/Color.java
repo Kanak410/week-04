0dd56f0461e87f5fff22a78e8f3fe2d32f33b170
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-15 15:05:25 +0530

    [kanak]added a program of regex of valid hexa color

---
+++ Day5_RegexAndJunit/src/main/java/com/regex/validatehexcolorCode/Color.java
@@ -0,0 +1,17 @@
+package com.regex.validatehexcolorCode;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Color {
+    public static boolean hex(String color){
+        String regex="^#([A-Fa-f0-9]{6})$";
+        return Pattern.matches(regex,color);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the color: ");
+        String color=sc.nextLine();
+        System.out.println(hex(color));
+    }
+}
