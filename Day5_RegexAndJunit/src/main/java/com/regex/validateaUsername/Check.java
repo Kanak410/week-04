2c51930eb88f0b3d99b9e3bd3f8b19ab30046ca6
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-15 14:01:14 +0530

    [kanak]added a program of regex of valid username

---
+++ Day5_RegexAndJunit/src/main/java/com/regex/validateaUsername/Check.java
@@ -0,0 +1,22 @@
+package com.regex.validateaUsername;
+import java.util.regex.*;
+import java.util.Scanner;
+public class Check {
+
+
+
+        public static boolean isValidUsername(String username) {
+            String regex = "^[a-zA-Z][a-zA-Z0-9_]{4,14}$";
+            return Pattern.matches(regex, username);
+        }
+
+        public static void main(String[] args) {
+            System.out.println("enter the username");
+            Scanner sc=new Scanner(System.in);
+            String username=sc.nextLine();
+            System.out.println(isValidUsername(username));
+
+        }
+
+
+}
