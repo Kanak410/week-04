f91dd92837d1ebc83db4d12c2cfe6a52af91c4b8
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-15 16:02:48 +0530

    [kanak]added a program of regex of capital word

--- Day5_RegexAndJunit/src/main/java/com/regex/exctractionproblems/capitalizedwords/Sentence.java
+++ Day5_RegexAndJunit/src/main/java/com/regex/exctractionproblems/capitalizedwords/Sentence.java
@@ -1,4 +1,24 @@
 package com.regex.exctractionproblems.capitalizedwords;
-
+import java.util.*;
+import java.util.regex.*;
 public class Sentence {
+    public static List<String> capital(String s){
+        List<String>sentence=new ArrayList<>();
+        String pattern="\\b[A-Z][a-z]*\\b";
+        Pattern regex=Pattern.compile(pattern);
+        Matcher matcher=regex.matcher(s);
+        while(matcher.find()){
+            sentence.add(matcher.group());
+        }
+
+        return sentence;
+    }
+
+    public static void main(String[] args) {
+        String s="The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+        List<String>capitalw=capital(s);
+        System.out.println(String.join(",",capitalw));
+
+    }
+
 }
