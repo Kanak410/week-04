2b57b4b168175bd11450c44325cb462ca48be8fe
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-16 13:15:34 +0530

    [kanak]Add .Added a program of testing calculator program

--- Day5_RegexAndJunit/src/main/java/com/junit/basic/testingaCalculatorClass/Calculator.java
+++ Day5_RegexAndJunit/src/main/java/com/junit/basic/testingaCalculatorClass/Calculator.java
@@ -1,4 +1,4 @@
-package com.junit.testingaCalculatorClass;
+package com.junit.basic.testingaCalculatorClass;
 
 public class Calculator {
     public int add(int a, int b) {
--- Day5_RegexAndJunit/src/main/java/com/junit/testingstringUtilityMethods/Utility.java
+++
@@ -1,4 +0,0 @@
-package com.junit.testingstringUtilityMethods;
-
-public class Utility {
-}
