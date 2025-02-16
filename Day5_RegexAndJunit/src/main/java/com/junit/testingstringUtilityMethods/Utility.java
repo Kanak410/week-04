695e09fe44d108f91c4b8d8d4b103b20babdf01d
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-16 13:02:45 +0530

    [kanak]Add .Added a program of testing calculator program

---
+++ Day5_RegexAndJunit/src/main/java/com/junit/testingstringUtilityMethods/Utility.java
@@ -0,0 +1,4 @@
+package com.junit.testingstringUtilityMethods;
+
+public class Utility {
+}
---
+++ Day5_RegexAndJunit/src/test/java/com/junit/testingaCalculatorClass/CalculaterTest.java
@@ -0,0 +1,34 @@
+package com.junit.testingaCalculatorClass;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CalculaterTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(4, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+        assertEquals("Cannot divide by zero", exception.getMessage());
+    }
+}
