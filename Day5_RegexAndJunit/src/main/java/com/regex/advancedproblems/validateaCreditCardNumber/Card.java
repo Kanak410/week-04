11ceb62ce3a1d96e1cdf75c605ae6e137fae08fe
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-16 09:57:02 +0530

    [kanak]Add .Added a program of credit card

---
+++ Day5_RegexAndJunit/src/main/java/com/regex/advancedproblems/validateaCreditCardNumber/Card.java
@@ -0,0 +1,36 @@
+package com.regex.advancedproblems.validateaCreditCardNumber;
+
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Card {
+    public static boolean checkVisaCard(String cardNumber){
+        String regex = "4\\d{15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(cardNumber);
+        return matcher.matches();
+    }
+    public static boolean checkMasterCard(String cardNumber){
+        String regex = "5\\d{15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(cardNumber);
+        return matcher.matches();
+    }
+    public static void main(String[] args) {
+        // Created a scanner for inputs
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the card number ");
+        String cardNumber = sc.nextLine().trim();
+        if(checkMasterCard(cardNumber)){
+            System.out.println("💳Its a Master Credit Card");
+
+        }
+        else if(checkVisaCard(cardNumber))
+            System.out.println("💳 Its a Visa Credit Card");
+        else System.out.println("Invalid Card Number");
+
+    }
+}
+
